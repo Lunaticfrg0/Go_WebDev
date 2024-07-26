1187e3734d4ef84d2064787456ced6814779cc3c
Emilio Escano <emilio3scano@gmail.com>
2024-07-25 20:02:28 -0400

    create simple server to show handler interface

--- httpPackage/main.go
+++ httpPackage/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+type helloWorldHandler int
+
+func (h helloWorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World from my http server!")
+}
 
 func main() {
-	fmt.Println("Commit test")
+	fmt.Println("Server started!")
+	var helloWorld helloWorldHandler
+	http.ListenAndServe(":8080", helloWorld)
 }
